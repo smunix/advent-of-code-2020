fa7e5a7576912ef826e70696aa3b93eef60a6407
Providence Salumu <Providence.Salumu@smunix.com>
2020-12-08 01:03:12 -0500

    D1, refactor: use ByteString

--- D1/src/D1/Run.hs
+++ D1/src/D1/Run.hs
@@ -10,19 +10,13 @@
 
 module D1.Run
   ( run,
-    part1,
-    part2,
   )
 where
 
-import Control.Monad.Extra (io)
-import D1.Import
-import RIO.List (nub)
-import qualified RIO.List as L
-import RIO.List.Partial (head)
-import RIO.Partial (read)
-import System.IO (hGetContents, print)
-import Utils.Import (search)
+import D1.Import hiding (words)
+import Data.ByteString.Lazy.Char8 (readInt, words)
+import RIO.ByteString.Lazy (hGetContents)
+import RIO.List (tails)
 
 {- https://adventofcode.com/2020/day/1 -}
 
@@ -55,43 +49,12 @@ Of course, your expense report is much larger. Find the two entries that sum to 
 Your puzzle answer was 55776.
 -}
 
-test1, test2 :: [] Int
-test1 =
-  [ 1721,
-    979,
-    366,
-    299,
-    675,
-    1456
-  ]
-test2 =
-  [ 1721,
-    979,
-    366,
-    299,
-    675,
-    1456
-  ]
-
-part1 :: Int -> [] Int -> (Int, Int)
-part1 total xs0 = search finished' refine' partial' & head
-  where
-    partial' = (xs0, (0, 0))
-    finished' (_, (a, b))
-      | a + b == total = Just (a, b)
-      | otherwise = Nothing
-    refine' ([], (_, _)) = mempty
-    refine' (x : xs, (_, _)) = xs <&> \y -> (xs, (x, y))
-
-part1'' :: Int -> [] Int -> Int
-part1'' total xs0 = [x | x <- xs0, y <- xs0, total == x + y] & nub & product
-
-solve :: [] Int -> [] Int
+solve :: [] Int -> [] ((Int, Int), Int)
 solve xs = do
-  x : ys <- L.tails xs
+  x : ys <- tails xs
   y <- ys
   guard $ 2020 == x + y
-  pure $ x * y
+  pure ((x, y), x * y)
 
 {-
 --- Part Two ---
@@ -106,45 +69,27 @@ Your puzzle answer was 223162626.
 
 -}
 
-part2' :: Int -> [] Int -> (Int, Int, Int)
-part2' total xs0 = search finished' refine' partial' & head
-  where
-    partial' = (xs0, (0, 0, 0))
-    finished' (_, (a, b, c))
-      | a + b + c == total = Just (a, b, c)
-      | otherwise = Nothing
-    refine' :: ([] Int, (Int, Int, Int)) -> [] ([] Int, (Int, Int, Int))
-    refine' partial =
-      if
-          | ([], (_, _, _)) <- partial -> mempty
-          | ([_, _], (_, _, _)) <- partial -> mempty
-          | ([x, y, z], (_, _, _)) <- partial -> pure (mempty, (x, y, z))
-          | (x : xs, (_, _, _)) <- partial -> ((,) <$> xs <*> xs <&> \(y, z) -> (xs, (x, y, z))) & nub
-
-part2 :: Int -> [] Int -> (Int, Int, Int)
-part2 total = head . triplet
-  where
-    triplet xs =
-      if
-          | [] <- xs -> mempty
-          | [_, _] <- xs -> mempty
-          | [x, y, z] <- xs -> pure (x, y, z)
-          | x : ys <- xs -> do
-            let yz = do
-                  y <- ys
-                  z <- ys
-                  pure (y, z)
-            ((yz & nub <&> \(y, z) -> guard (total == x + y + z) >> pure (x, y, z)) & join & nub) <> triplet ys
-
-part2'' :: Int -> [] Int -> Int
-part2'' total xs0 = (\x y z -> guard (total == x + y + z) >> pure x) <$> xs0 <*> xs0 <*> xs0 & join & nub & product
+solve' :: [] Int -> [] ((Int, Int, Int), Int)
+solve' xs = do
+  x : ys <- tails xs
+  y : zs <- tails ys
+  z <- zs
+  guard $ 2020 == x + y + z
+  pure ((x, y, z), x * y * z)
 
 run :: RIO App ()
 run = do
-  logInfo "We're inside the application!"
-  logInfo $ fromString $ show $ solve test1
-  io do
-    withFile "D1/assets/inputs.txt" ReadMode \h -> do
-      contents <- hGetContents h
-      let ints = contents & words <&> (read @Int)
-      part2'' 2020 ints & print
+  let test :: [] Int
+      test =
+        [ 1721,
+          979,
+          366,
+          299,
+          675,
+          1456
+        ]
+  logDebug . fromString $ ("part 1: " <> (show $ solve test))
+  withFile "D1/assets/inputs.txt" ReadMode \h -> do
+    contents <- hGetContents h
+    let ints = contents & words <&> readInt & catMaybes <&> fst
+    logDebug . fromString $ ("part 2: " <> (show $ solve' ints))
